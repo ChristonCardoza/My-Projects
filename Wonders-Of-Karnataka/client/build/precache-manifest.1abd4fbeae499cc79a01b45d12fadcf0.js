b52b910e7dca8afc30ac67eecce908b3ab84ed97
Christon Cardoza <christon.antony_cardoza@rsa.com>
2021-08-18 00:38:22 +0530

    Wonders of karnataka :)

---
+++ Wonders-Of-Karnataka/client/build/precache-manifest.1abd4fbeae499cc79a01b45d12fadcf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59586a833ee817abc43dbe31ba25003b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22aa01d8cb4ce2a24400",
+    "url": "/static/css/main.9516a8fc.chunk.css"
+  },
+  {
+    "revision": "430eb69bafe91ef83f84",
+    "url": "/static/js/2.cc5581a8.chunk.js"
+  },
+  {
+    "revision": "c06d497d6716e84111c2fd9253bcfc02",
+    "url": "/static/js/2.cc5581a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22aa01d8cb4ce2a24400",
+    "url": "/static/js/main.590327aa.chunk.js"
+  },
+  {
+    "revision": "5f4bbedf92f9004e7c56",
+    "url": "/static/js/runtime-main.1507d376.js"
+  }
+]);
